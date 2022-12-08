cef1f1e8f75ed0d5b72184453850c375db2351d6
Sadiq <seeker99157@gmail.com>
2022-12-08 17:46:12 +0100

    built sidebar and app header

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,34 +6,46 @@ import { ThemeContext } from '../App'
 import Header from './Header'
 import Sidebar from './Sidebar'
 
+// Icons
+
+import { AiOutlineEyeInvisible } from 'react-icons/ai'
+import { AiOutlineEye } from 'react-icons/ai'
+
+
 const Home = () => {
     const { theme, sideBar } = useContext(ThemeContext)
     const [input, setInput] = useState()
+    const [preview, setPreview] = useState(false)
 
     return (
-        <div className='flex '>
+        <div className='flex'>
             {/* Sidebar */}
             <div>
                 <Sidebar />
             </div>
 
-            <div className={sideBar ? 'ml-[200px] w-full ease-in-out duration-300' : 'w-full ease-in-out duration-300'}>
+            <div className={sideBar ? 'ml-[250px] w-full ease-in-out duration-300 h-screen' : 'w-full ease-in-out duration-300 h-screen'}>
                 <Header />
                 <div className='flex'>
                     {/* Markdown text area */}
-                    <div className='w-1/2'>
+                    <div className='w-1/2 h-full'>
+                        <h2 className='py-2 pl-5 font-semibold text-base tracking-widest bg-[#1d1f22] text-[#7c8187] border-r border-r-[#afb2b4]'>MARKDOWN</h2>
                         <textarea
                             autoFocus
                             placeholder='Text text'
                             value={input}
                             onChange={(e) => setInput(e.target.value)}
-                            className='w-1/2 min-h-screen p-5 text-xl outline-none'></textarea>
+                            className='w-full h-full p-5 text-xl outline-none break-words'></textarea>
                     </div>
 
                     {/* Rendered JSX */}
                     <div className='w-1/2'>
+                        <div className='flex items-center justify-between py-2 px-5 font-semibold text-base tracking-widest bg-[#1d1f22] text-[#afb2b4]'><p>PREVIEW</p>
+                            <button onClick={() => setPreview(!preview)}>{preview ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
+                            </button></div>
+
                         <ReactMarkdown
-                            className='w-1/2 border-l h-screen overflow-y-scroll'
+                            className='border-l overflow-y-scroll overflow-x-scroll break-words'
                             components={{
                                 code({ node, inline, className, children, ...props }) {
                                     const match = /language-(\w+)/.exec(className || "")
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,15 +1,42 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ThemeContext } from "../App"
+import { FiPlus } from 'react-icons/fi'
+import { HiOutlineMoon } from 'react-icons/hi'
+import { HiOutlineSun } from 'react-icons/hi'
+import Switch from './Switch'
 
 const Sidebar = () => {
-    const { sideBar } = useContext(ThemeContext)
+    const { sideBar, toggleTheme, theme } = useContext(ThemeContext)
+    // console.log(theme)
+    const [value, setValue] = useState(false)
+
+    const switchTheme = () => {
+        setValue(!value)
+        toggleTheme()
+    }
     return (
         <div className={
             sideBar
-                ? 'absolute top-0 left-0 right-0 bottom-0 h-screen w-[200px] bg-black ease-in-out duration-300'
-                : 'absolute top-0 left-[-100%] w-full h-screen  bg-black ease-in-out duration-300'
+                ? 'absolute top-0 left-0 right-0 bottom-0 h-screen w-[250px] bg-[#1d1f22] ease-in-out duration-300'
+                : 'absolute top-0 left-[-100%] w-full h-screen  bg-[#1d1f22] ease-in-out duration-300'
         }>
-            <p className="text-white">Sidebar.</p>
+            <h2 className="pl-5 py-5 text-[#c1c4cb] text-base font-semibold capitalize leading-5 tracking-widest">MY DOCUMENTS</h2>
+            <div className="grid  mx-5 h-[80vh]">
+                <div><button className="flex justify-center items-center bg-[#f06820] py-1.5 rounded-md text-white h-auto w-full"><FiPlus size={17} /> New Document</button></div>
+
+
+                <div className="flex self-end items-center">
+                    <HiOutlineMoon className={theme === 'light' ? 'text-gray-400 mr-2' : 'text-white mr-2'} />
+                    <Switch
+                        isOn={value}
+                        handleSwitch={switchTheme}
+                    />
+                    <HiOutlineSun className={theme === 'dark' ? 'text-gray-400 ml-2' : 'text-white ml-2'} />
+
+                </div>
+
+            </div>
+
         </div>
     )
 }
